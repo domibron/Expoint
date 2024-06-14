1797ecca3b7ca100082929f8a5496afffa283ddd
DM <dommcneill@outlook.com>
2024-06-14 01:23:31 +0100

    attempting to network

--- Expoint/Assets/Scripts/Networking/CustomNetworkManager.cs
+++ Expoint/Assets/Scripts/Networking/CustomNetworkManager.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class CustomNetworkManager : NetworkManager
+public class CustomNetworkManager : NetworkRoomManager
 {
+	public override void Start()
+	{
+		base.Start();
+	}
 
+	public void ConnectToIPAdress()
+	{
+		string MyAttualIP = "86.10.14.161";
+		NetworkClient.Connect(MyAttualIP);
+	}
 }
